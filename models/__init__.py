e0d0f961a4e40d88d1b39485533f47f93ee8686a
Ernst <ernstroell@gmail.com>
2023-08-08 16:17:40 +0200

    fixed requirements

--- models/__init__.py
+++ models/__init__.py
@@ -5,7 +5,6 @@ from abc import ABC, abstractmethod
 import torch
 import torch.nn.functional as F
 from torch.utils.data import random_split
-import pytorch_lightning as pl
 
 """
 This package includes all the modules related to data loading and preprocessing.
@@ -16,21 +15,18 @@ The below code fetches all files in the datasets folder and imports them. Each
 dataset can have its own file, or grouped in sets.
 """
 path = os.path.dirname(os.path.abspath(__file__))
-for py in [f[:-3] for f in os.listdir(path) if f.endswith('.py') and f != '__init__.py']:
-    mod = __import__('.'.join([__name__, py]), fromlist=[py])
+for py in [
+    f[:-3] for f in os.listdir(path) if f.endswith(".py") and f != "__init__.py"
+]:
+    mod = __import__(".".join([__name__, py]), fromlist=[py])
     classes = [getattr(mod, x) for x in dir(mod) if isinstance(getattr(mod, x), type)]
     for cls in classes:
         setattr(sys.modules[__name__], cls.__name__, cls)
 
 
-def load_model(name,config):
-    model = locate(f'models.{name}')
+def load_model(name, config):
+    model = locate(f"models.{name}")
     if not model:
         print(f"Tried to load model {name}")
         raise AttributeError()
     return model(config)
-
-
-
-
-
--- models/custom_model.py
+++ models/custom_model.py
@@ -1,16 +1,12 @@
-
 import torch
 import torch.nn.functional as F
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 from torch import nn, optim
 from torch.utils.data import DataLoader
-from tqdm import tqdm
-import pytorch_lightning as pl
-import torchmetrics
-from torchmetrics import Metric
 import torchvision
 from models.base_model import BaseModel
+
 
 class NN(BaseModel):
     def __init__(self, config):
@@ -19,14 +15,8 @@ class NN(BaseModel):
         self.fc1 = nn.Linear(config.input_size, 1_00)
         self.fc2 = nn.Linear(1_00, config.num_classes)
         self.loss_fn = nn.CrossEntropyLoss()
-        self.accuracy = torchmetrics.Accuracy(
-            task="multiclass", num_classes=config.num_classes
-        )
-        self.f1_score = torchmetrics.F1Score(task="multiclass", num_classes=config.num_classes)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
-
-
