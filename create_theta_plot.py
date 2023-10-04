ba26c7ea395b2be8a65043899182f8c412811803
Ernst <ernstroell@gmail.com>
2023-10-04 15:24:36 +0200

    Cleaning

--- create_theta_plot.py
+++
@@ -1,8 +0,0 @@
-from vedo import *
-import torch
-import vedo
-
-thetas = torch.load("before.pt")
-sphere = vedo.shapes.Sphere(pos=(0, 0, 0), r=1, alpha=0.2)
-
-Points(thetas.cpu().detach().numpy()).show(axes=1).close()
