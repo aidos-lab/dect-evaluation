52c2e273d8b0c88b77c00273550755b9bdedf944
Ernst <ernstroell@gmail.com>
2023-10-04 20:24:47 +0200

    removed redundant models

--- models/config.py
+++ models/config.py
@@ -2,13 +2,16 @@ from dataclasses import dataclass
 
 
 @dataclass(frozen=True)
-class ECTModelConfig:
-    module: str
-    num_phis: int = 12
-    num_thetas: int = 13
-    bump_steps: int = 14
-    batch_size: int = 128
+class EctConfig:
+    num_thetas: int = 32
+    bump_steps: int = 32
     R: float = 1.1
+
+
+@dataclass(frozen=True)
+class ModelConfig:
+    module: str
+    ectconfig: EctConfig = EctConfig()
     num_features: int = 3
     num_classes: int = 10
     hidden: int = 50
