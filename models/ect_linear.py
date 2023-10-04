55c5a83ca79233c8059892f0340efad5adf4fd28
Ernst <ernstroell@gmail.com>
2023-10-04 21:02:32 +0200

    Cleaned model class

---
+++ models/ect_linear.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+from models.base_model import BaseModel
+
+from models.layers.layers import EctLayer
+from models.config import EctConfig
+
+
+class EctLinearModel(BaseModel):
+    def __init__(self, config: EctConfig):
+        super().__init__(config)
+        self.ectlayer = EctLayer(config.ectconfig)
+
+        self.linear = nn.Sequential(
+            nn.Linear(self.config.num_thetas * self.config.bump_steps, config.hidden),
+            nn.ReLU(),
+            nn.Linear(config.hidden, config.hidden),
+            nn.ReLU(),
+            nn.Linear(config.hidden, config.num_classes),
+        )
+
+    def forward(self, batch):
+        x = self.ectlayer(batch).reshape(
+            -1, self.config.num_thetas * self.config.bump_steps
+        )
+        x = self.linear(x)
+        return x
+
+
+from loaders.factory import register
+
+
+def initialize():
+    register("model", EctLinearModel)
